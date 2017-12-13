f762b2fc7eca96538ed4416d5f54dca5161a44b0
albertolei <albertolei@qq.com>
2017-12-13 14:04:43 +0800

    20171213测试C#中文变量名和中文函数名

---
+++ CsharpTestChineseVariable/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpTestChineseVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test t = new Test();
+            t.名称 = "雷刚";
+            t.修改名称("alberto");
+            Console.WriteLine(t.名称);
+            Console.Read();
+        }
+    }
+    class Test
+    {
+        public string 名称;
+        public void 修改名称(string 新名称)
+        {
+            this.名称 = 新名称;
+        }
+    }
+}
